4893add540f90e7d1e38820d1e46050d72ed3265
Dan Ruswick <dan.ruswick@gmail.com>
2019-12-06 15:58:35 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,78 @@
+class Observable {
+    constructor() {
+        this.pipes = []
+    }
+
+    dip(fn) {
+        this.pipe((v) => {
+            fn(v)
+            return v
+        })
+    }
+
+    async emit(v) {
+        if (v instanceof Array) {
+            for (let i of v) {
+                this.emit(i)
+            }
+        } else {
+            this.pipes.reduce((_v, fn) => fn(_v), v)
+        }
+    }
+    pipe(fn) {
+        this.pipes.push(fn)
+        return this
+    }
+
+    debug() {
+        this.pipe(v => {
+            console.log(v)
+            return v
+        })
+        return this
+    }
+
+    reduce(fn, initial_state) {
+        let _acc = initial_state
+        let _fn = fn
+        this.pipe(v => {
+            _acc = _fn(_acc, v)
+            return _acc
+        })
+        return this
+    }
+
+    timer(ms) {
+        const timer = setInterval(() => {
+            this.emit(Date.now())
+        }, ms)
+        return this
+    }
+
+    store() {
+        this.reduce((a, v) => {
+            a.push(v)
+            return a
+        }, [])
+        return this
+    }
+    listen() {
+        const new_o = new Observable()
+        this.pipe(v => {
+            new_o.emit(v)
+            return v
+        })
+        return new_o
+    }
+
+    kill() {
+        this.pipes = []
+    }
+}
+
+export default Observable
+
+// let t = new Observable()
+// t.timer(1000)
+// t.listen().store().pipe(v => `val: ${v}`).debug()
+// t.listen().pipe(v => v / v).debug()
