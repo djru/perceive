5dc56a32e6f76633cb349beb48f91a20b92b9f55
Dan Ruswick <dan.ruswick@gmail.com>
2019-12-07 14:18:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -76,22 +76,3 @@ export default Observable
 // t.timer(1000)
 // t.listen().store().pipe(v => `val: ${v}`).debug()
 // t.listen().pipe(v => v / v).debug()
-
-
-const m_height = window.innerHeight
-const m_width = window.innerWidth
-
-const m_rad = Math.ceil((m_height ** 2 + m_width ** 2) ** .5)
-const rad = Math.min(m_height, m_width)
-
-
-const div = document.querySelector('#main')
-div.style.width = `${rad}px`
-div.style.height = `${rad}px`
-const m_scale = m_rad / rad
-const start = Date.now()
-setInterval(() => {
-    percent_elapsed = Math.abs(((Date.now() - start) % 2000) - 1000) / 1000
-    div.style.transform = `scale(${m_scale * percent_elapsed})`
-
-}, 10)
