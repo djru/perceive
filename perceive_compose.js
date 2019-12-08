61ba21d70fd39a2c7e2cfbb373106695054b003f
Dan Ruswick <dan.ruswick@gmail.com>
2019-12-07 18:42:36 -0600

    new everything

--- perceive_compose.js
+++ perceive_compose.js
@@ -4,33 +4,23 @@ const filter_obs = (a) => a.filter(a => a instanceof Observable)
 // need to wrap lock in an object because booleans are pass by value in js
 const generate_lock = (initial_value=false) => ({locked: initial_value, lock: function(){this.locked = true}, unlock: function(){this.locked = false}})
 // template that creates lock, filters observables, manages state and passes to a callback
-const compose = (callback) => {
-    return (..args) => {
-        args = filter_obs(args)
-        let state = Array(args.length)
-        let lock = generate_lock()
-        let output = new Observable()
-        callback({args, output, state, lock})
-    }
-    
-}
 
 // common logic is abstracted here. This is used when you want to subscribe to many things and do something with the value. It takes a callback and passes the value, state lock and i 
 const multiplex = (callback) => (...args) => {
     args = filter_obs(args)
-    let state = Array(args.length)
+    let state = Array(args.length).fill(null)
     let lock = generate_lock()
     let output = new Observable()
     let i = 0
     for (let o of args) {
         const _i = i
-        o.subscribe((v) => {
+        o.subscribe(v => {
             callback({v, output, lock, state, i: _i})
-            
-        }
+        })
         i += 1
     }
-})
+    return output
+}
 
     
 
@@ -47,14 +37,18 @@ export const race = multiplex(({v, lock, output}) => {
 
 export const buffer = multiplex(({v, output, state, i}) => {
     state[i] = v
-    if(!state.find(undefined)){
-        new_o.emit(state)
-        state = Array(state.length)
+    if(state.every(_v => !!_v)){
+        output.emit(state)
+        for(let i in state){
+            state[i] = null
+        }
     }
 })
 
 
 export const pairwise = multiplex(({v, output, state, i}) => {
     state[i] = v
-    new_o.emit(state)
+    if(state.every(_v => !!_v)){
+        output.emit(state)
+    }
 })
