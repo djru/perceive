9ebabc07ddc5a64fb6d696ec4862b80dc484b462
Dan Ruswick <dan.ruswick@gmail.com>
2019-12-10 20:10:11 -0600

    Update perceive.js

--- perceive.js
+++ perceive.js
@@ -1,84 +1,138 @@
-class Observable {
-    constructor() {
-        this.pipes = []
+export default class Observable {
+    constructor(source = null) {
+        this.subscribers = []
+        this.source = parent
+    }
+    subscribe(callback) {
+        let active = true
+        this.subscribers.push(v => {
+            if (active) {
+                // want the subscriber propogation to be async, so use requestIdleCallback
+                requestIdleCallback(() => {
+                    callback(v)
+                })
+            }
+        })
+        return () => {
+            active = false
+        }
     }
 
-    subscribe(fn) {
-        this.pipe((v) => {
-            fn(v)
-            return v
-        })
+    getRoot() {
+        return this.source ? this.source.getRoot() : this
     }
 
     emit(v) {
         if (v instanceof Array) {
-            for (let i of v) {
-                this.emit(i)
-            }
+            v.map(_v => this.emit(_v))
         } else {
-            this.pipes.reduce((_v, fn) => fn(_v), v)
+            for (let s of this.subscribers) {
+                s(v)
+            }
         }
+
     }
-    pipe(fn, _prepend=false) {
-        if(_prepend){
-            this.pipes.unshift(fn)
-        }
-        else{
-            this.pipes.push(fn)
-        }
-        return this
+
+    clear() {
+        this.subscribers = []
+    }
+
+    pipe(...pipes) {
+        const o = new Observable(this)
+        this.subscribe(v => {
+            const piped_value = pipes.reduce((a, f) => f(a), v)
+            o.emit(piped_value)
+        })
+        return o
+    }
+
+    debounce(t) {
+        let ts = Date.now()
+        const o = new Observable(this)
+        this.subscribe(v => {
+            if (Date.now() > ts + t) {
+                ts = Date.now()
+                o.emit(v)
+            }
+        })
+        return o
+    }
+
+    reduce(callback, init_val) {
+        let stored_val = init_val
+        const o = new Observable(this)
+        this.subscribe(v => {
+            stored_val = callback(stored_val, v)
+            o.emit(stored_val)
+        })
+        return o
     }
 
     debug() {
-        this.pipe(v => {
-            console.log(...arguments, v)
-            return v
+        const o = new Observable(this)
+        this.subscribe(v => {
+            console.log(v)
+            o.emit(v)
         })
-        return this
-    }
-
-    reduce(fn, initial_state) {
-        let _acc = initial_state
-        let _fn = fn
-        this.pipe(v => {
-            _acc = _fn(_acc, v)
-            return _acc
-        })
-        return this
-    }
-
-    timer(ms) {
-        const timer = setInterval(() => {
-            this.emit(Date.now())
-        }, ms)
-        return this
+        return o
     }
 
     collect() {
-        this.reduce((a, v) => {
-            a.push(v)
-            return a
-        }, [])
-        return this
-    }
-    listen() {
-        const new_o = new Observable()
-        this.pipe(v => {
-            new_o.emit(v)
-            return v
+        const o = new Observable(this)
+        const state = []
+        this.subscribe(v => {
+            state.push(v)
+            o.emit(state)
         })
-        return new_o
+        return o
     }
-    copy() {
-        const new_o = new Observable()
-        this.pipe(v => {
-            new_o.emit(v)
-            return v
-        }, true)
-        return new_o
-    }
+}
 
-    kill() {
-        this.pipes = []
+
+export const merge = (...observables) => {
+    const o = new Observable()
+    for (let _o of observables) {
+        _o.subscribe(v => {
+            o.emit(v)
+        })
     }
+    return o
+}
+export const race = (...observables) => {
+    const o = new Observable()
+    let lock = false
+    for (let _o of observables) {
+        _o.subscribe(v => {
+            if (!lock) {
+                lock = true
+                o.emit(v)
+            }
+        })
+    }
+    return o
+}
+
+export const combinedLatest = (...observables) => {
+    let state = Array(observables.length).fill(null)
+    const o = new Observable()
+    const i = 0
+    for (let _o of observables) {
+        let _i = i
+        _o.subscribe(v => {
+            state[_i] = v
+            if (state.every(v => v !== null)) {
+                o.emit(state)
+                state = Array(observables.length).fill(null)
+            }
+        })
+    }
+    return o
+}
+
+export const timer = (ms) => {
+    const o = new Observable()
+    setInterval(() => {
+        o.emit(Date.now())
+    }, ms)
+    return o
 }
